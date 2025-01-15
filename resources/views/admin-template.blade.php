e21c339550483d3365848033103ce958fee08c0d
KhinThetLin <khinthetlin2018@gmail.com>
2025-01-15 14:11:58 +0630

    latest code

--- resources/views/admin-template.blade.php
+++ resources/views/admin-template.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Essential School</title>
+    <title>Event Api</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link href="{{asset('admin/css/bootstrap.min.css')}}" rel="stylesheet">
